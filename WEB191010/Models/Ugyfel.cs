19d849a3c21590bbca58533bd172f5fdfb987ef5
Szűcs Richárd <szucsricsi12@gmail.com>
2019-10-18 16:27:33 +0200

    Adatbázis változtatás

--- WEB191010/Models/Ugyfel.cs
+++ WEB191010/Models/Ugyfel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,9 @@ namespace WEB191010.Models
     public class Ugyfel
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+
         public string Nev { get; set; }
         public bool HirlevelFeliratkozas { get; set; }
         public ElofizetesTipus ElofizetesTipus { get; set; }
